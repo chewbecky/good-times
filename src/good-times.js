3fe5bba4648164c6d4cf71044895a70cfe1bf63c
chewbecca <beckybettinger@yahoo.de>
2020-02-14 12:02:27 +0100

    with webview

---
+++ src/good-times.js
@@ -0,0 +1,73 @@
+import sketch from "sketch";
+import util from "util";
+const { DataSupplier } = sketch;
+const document = sketch.getSelectedDocument();
+var UI = require('sketch/ui')
+const timePeriods = {
+  "Week in Future": 604800,
+  "Days in Future": -86400,
+  "Hours in Future": -3600,
+  "Minutes in Future": -60,
+  "Seconds in Future": 5,
+  "Seconds ago": 5,
+  "Minutes ago": 60,
+  "Hours ago": 3600,
+  "Days ago": 86400,
+  "Week ago": 604800,
+};
+
+export function onStartup() {
+  // To register the plugin, uncomment the relevant type:
+  DataSupplier.registerDataSupplier("public.text", "GoodTimes", "SupplyData");
+  // DataSupplier.registerDataSupplier('public.image', 'GoodTimes', 'SupplyData')
+}
+
+export function onShutdown() {
+  // Deregister the plugin
+  DataSupplier.deregisterDataSuppliers();
+}
+
+export function onSupplyData(context) {
+
+  UI.getInputFromUser(
+    "Please specify the desired period of time",
+    {
+      type: UI.INPUT_TYPE.selection,
+      possibleValues: Object.keys(timePeriods)
+    },
+    (err, value) => {
+      if (err) {
+        // most likely the user canceled the input
+        return;
+      }
+      if (value) {
+        let dataKey = context.data.key;
+        const originalDataItems = util
+            .toArray(context.data.items)
+            .map(sketch.fromNative);
+        const selection = document.selectedLayers.layers;
+        let items = [];
+
+        originalDataItems.forEach((odi, index) => {
+          let item = {};
+          item.layer = selection[index];
+          item.originalIndex = index;
+          items.push(item);
+        });
+
+        console.log(items, selection);
+
+        items.sort((a, b) => a.layer.frame.y - b.layer.frame.y);
+
+        console.log(items);
+
+        items.forEach((item, index) => {
+          let data = new Date(Date.now() - (index * timePeriods[value]*1000)).toLocaleString();
+          DataSupplier.supplyDataAtIndex(dataKey, data, item.originalIndex);
+        });
+      }
+    }
+  );
+
+
+}
