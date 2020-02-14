919c9b43ea5ecb9ce4f7fb46fb333b609139db24
chewbecca <beckybettinger@yahoo.de>
2020-02-14 12:02:53 +0100

    webview

--- src/good-times.js
+++ src/good-times.js
@@ -1,8 +1,11 @@
 import sketch from "sketch";
 import util from "util";
+import BrowserWindow from "sketch-module-web-view";
+import { getWebview } from 'sketch-module-web-view/remote'
+
 const { DataSupplier } = sketch;
 const document = sketch.getSelectedDocument();
-var UI = require('sketch/ui')
+var UI = require("sketch/ui");
 const timePeriods = {
   "Week in Future": 604800,
   "Days in Future": -86400,
@@ -13,8 +16,10 @@ const timePeriods = {
   "Minutes ago": 60,
   "Hours ago": 3600,
   "Days ago": 86400,
-  "Week ago": 604800,
+  "Week ago": 604800
 };
+
+const webviewIdentifier = "{{ slug }}.webview";
 
 export function onStartup() {
   // To register the plugin, uncomment the relevant type:
@@ -25,11 +30,16 @@ export function onStartup() {
 export function onShutdown() {
   // Deregister the plugin
   DataSupplier.deregisterDataSuppliers();
+  const existingWebview = getWebview(webviewIdentifier);
+  if (existingWebview) {
+    existingWebview.close();
+  }
 }
-
 export function onSupplyData(context) {
 
-  UI.getInputFromUser(
+    console.log("test");
+
+  /* UI.getInputFromUser(
     "Please specify the desired period of time",
     {
       type: UI.INPUT_TYPE.selection,
@@ -67,7 +77,43 @@ export function onSupplyData(context) {
         });
       }
     }
-  );
+  );*/
+}
 
+export default function () {
+    console.log("test");
+
+  const options = {
+    identifier: webviewIdentifier,
+    width: 800,
+    height: 600,
+    show: false
+  };
+
+  const browserWindow = new BrowserWindow(options);
+
+  // only show the window when the page has loaded to avoid a white flash
+  browserWindow.once("ready-to-show", () => {
+    browserWindow.show();
+  });
+
+  const webContents = browserWindow.webContents;
+
+  // print a message when the page loads
+  webContents.on("did-finish-load", () => {
+    UI.message("UI loaded!");
+  });
+
+  // add a handler for a call from web content's javascript
+  webContents.on("nativeLog", s => {
+    UI.message(s);
+    webContents
+        .executeJavaScript(`setRandomNumber(${Math.random()})`)
+        .catch(console.error);
+  });
+
+  browserWindow.loadURL(require("../resources/webview.html"));
 
 }
+
+
